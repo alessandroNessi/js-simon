6c310aa52180324256f7d9dbd1808e00f61364e3
alessandroNessi <alessandronessi@msn.com>
2021-09-07 11:13:51 +0200

    fixato un bug sul rigioca relativo al timeout

--- script/script.js
+++ script/script.js
@@ -33,6 +33,8 @@ function checkArray(string, array, cut){
     return false;
 }
 document.getElementById("play").addEventListener("mousedown",function(){
+    aiNumbers.length=0;
+    userNumbers.length=0;
     let cards=document.getElementsByClassName("cardFrame");
     document.getElementById("play").classList.add("d-none");
     document.getElementById("results").classList.add("d-none");
@@ -45,7 +47,6 @@ document.getElementById("play").addEventListener("mousedown",function(){
         cards[i].getElementsByClassName("number")[0].innerHTML=temp;
         aiNumbers.push(temp);
     }
-    
     /*flipping cards frontally*/    
     flipCards(cards,"front");
 
@@ -55,30 +56,38 @@ document.getElementById("play").addEventListener("mousedown",function(){
     },timeout);
 
     setTimeout(function(){
-        let result=1, num, i=0;
         document.getElementById("inpuNumber").classList.remove("d-none");
         document.getElementById("check").classList.remove("d-none");
-        document.getElementById("check").addEventListener("mousedown", function(){
-            num=parseInt(document.getElementById("inpuNumber").value);
-            console.log(num);
-            if(checkArray(num,aiNumbers,true)){
-                userNumbers.push(num);
-                i++;
-            }else{
-                result=0;
-            }
-            if(result==1 && aiNumbers.length==0){
-                resumeClass();
-                document.getElementById("results").innerHTML="Gj, you win!";
-            }else if(result==0){
-                let stringNumbers=userNumbers.join(" , ");
-                resumeClass();
-                document.getElementById("results").innerHTML=`You lost! Number "${num}" isn't in the cards! ${userNumbers.length}/5 have been insered correctly: ${stringNumbers};`;
-            }else{
-                document.getElementById("inpuNumber").value="";
-            }
-        });
     },timeout+600);
+});
+
+/* checks the numbers */
+document.getElementById("check").addEventListener("click", function(){
+    var result=1, num, i=0;
+    num = parseInt(document.getElementById("inpuNumber").value);
+    console.log("num "+num);
+    console.log("num "+ document.getElementById("inpuNumber").value);
+    console.log("result "+result);
+    console.log("aiNumbers.length "+aiNumbers.length);
+    document.getElementById("inpuNumber").value="";
+    if(checkArray(num,aiNumbers,true)){
+        userNumbers.push(num);
+        i++;
+    }else{
+        result=0;
+    }
+    if(result==1 && aiNumbers.length==0){
+        resumeClass();
+        document.getElementById("results").innerHTML="Gj, you win!";
+    }else if(result==0){
+        let stringNumbers=userNumbers.join(" , ");
+        resumeClass();
+        document.getElementById("results").innerHTML=`You lost! Number "${num}" isn't in the cards! ${userNumbers.length}/5 have been insered correctly: ${stringNumbers};`;
+        result=1;
+    }
+    console.log("num "+num);
+    console.log("result "+result);
+    console.log("aiNumbers.length "+aiNumbers.length);
 });
 function resumeClass(){
     document.getElementById("play").classList.remove("d-none");
